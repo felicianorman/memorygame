d38e540cd6fc7b4d315d1c5c261278b4c4a5c1f2
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-12-11 14:42:04 +0100

    finslipade flip funktionen och skapade poäng

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,12 +1,54 @@
-let memoryCard = document.querySelectorAll(".memory__card");
+let memoryCard = document.querySelectorAll(".card");
+
+let flippedCard = false;
+let firstCard: any, secondCard: any;
+let points = 0;
 
 //Skapar event listener
-for(let i = 0; i < memoryCard.length; i++) {
-    memoryCard[i].addEventListener("click", flip);
+for (let i = 0; i < memoryCard.length; i++) {
+  memoryCard[i].addEventListener("click", flip);
 }
 
 //Funktion för att flippa korten
 function flip(this: any) {
-    console.log("Klickad");
-    this.classList.add("flipped");
+  console.log("Kortet flippat");
+  this.classList.add("flipped");
+
+  //Om inget card flippas
+  if (!flippedCard) {
+    flippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  flippedCard = false;
+
+  checkMatch();
 }
+
+function checkMatch() {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    console.log("En match!");
+    points++;
+    console.log(points);
+  }
+
+  if(points === 6) {
+    console.log("High Score!");
+  }
+
+  noMatch();
+}
+
+function noMatch(this: any) {
+  if (firstCard.dataset.framework !== secondCard.dataset.framework) {
+    console.log("Ingen match");
+
+    setTimeout(() => {
+      firstCard.classList.remove("flipped");
+      secondCard.classList.remove("flipped");
+    }, 1000);
+  }
+}
+
